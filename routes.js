c345585abe4577d0858b1d445e701bd0925cf6ce
Arthaud Proust <arthaudp33@gmail.com>
2020-07-19 17:27:36 +0200

    add download app android

--- routes.js
+++ routes.js
@@ -6,24 +6,25 @@ module.exports = function(router) {
 
     // middleware that is specific to this router
     router.use(function timeLog(req, res, next) {
-        console.log((new Date()).toLocaleTimeString('fr-FR', {minute: '2-digit', hour: '2-digit', second:'2-digit'})+': '+req.path);
+        // console.log((new Date()).toLocaleTimeString('fr-FR', {minute: '2-digit', hour: '2-digit', second:'2-digit'})+': '+req.path);
+        console.log(req.path);
         next();
     });
 
-    router.get('/', function(res) {
+    router.get('/', function(req, res) {
         res.sendFile(path.join(__dirname+'/views/index.html'));
     });
-    // router.get('/create', function(req, res) {
-    //     res.redirect('/'+messages.newRoom());
-    // });
-
-    // router.get('/404', function(req, res) {
-    //     res.sendFile(path.join(__dirname+'/views/404.html'));
-    // });
-
+    router.get('/download', function(req, res) {
+        res.sendFile(path.join(__dirname+'/views/download.html'));
+    });
+    router.get('/download-android', function(req, res) {
+        res.download(path.join(__dirname+'/public/files/chat.apk'));
+    });
+    router.get('/download-ios', function(req, res) {
+        res.download(path.join(__dirname+'/public/files/chat.apk'));
+    });
 
     router.get('/:room', function(req, res) {
-        // console.log('Room',req.params.room);
         res.sendFile(path.join(__dirname+'/views/chat.html'));
         // if(messages.rooms.includes(req.params.room)) {
         // } else {
